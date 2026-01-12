6b0edda9960f2f438594177cf72629477ff2db90
Olimpialig <71033826+Olimpialig@users.noreply.github.com>
2026-01-11 22:08:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+let datos = JSON.parse(localStorage.getItem("crm")) || [];
+const tabla = document.getElementById("tabla");
+
+function render() {
+  tabla.innerHTML = "";
+  datos.forEach(d => {
+    tabla.innerHTML += `
+      <tr>
+        <td>${d.nombre}</td>
+        <td>${d.telefono}</td>
+        <td>${d.provincia}</td>
+      </tr>
+    `;
+  });
+}
+
+document.getElementById("crmForm").addEventListener("submit", e => {
+  e.preventDefault();
+
+  const cliente = {
+    nombre: nombre.value,
+    telefono: telefono.value,
+    provincia: provincia.value
+  };
+
+  datos.push(cliente);
+  localStorage.setItem("crm", JSON.stringify(datos));
+  render();
+  e.target.reset();
+});
+
+render();
 
